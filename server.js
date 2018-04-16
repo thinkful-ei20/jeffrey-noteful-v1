d9024917426833c136e429a9db66eefb8c7114cd
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-16 15:39:58 -0500

    Update the client and server to support a search

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const data = require('./db/notes');
 const app = express();
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => res.json(data));
+app.get('/api/notes', (req, res) => {
+  let results = data.slice();
+
+  if(req.query.searchTerm) {
+    results = results.filter(item => item.title.toUpperCase().includes(req.query.searchTerm.toUpperCase()));
+  }
+
+  res.json(results);
+});
 
 app.get('/api/notes/:id', (req, res) => {
   res.json(data.find(item => item.id === parseInt(req.params.id)));
