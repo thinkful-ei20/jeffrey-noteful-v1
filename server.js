22675f28f5a1bbe1be9cf77a621598efd0d53766
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-16 14:17:44 -0500

    Create Express App and setup a Static Server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 'use strict';
 
+const express = require('express');
+
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express();
+app.use(express.static('public'));
 
-// INSERT EXPRESS APP CODE HERE...
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`); // eslint-disable-line no-console
+}).on('error', err => {
+  console.error(err); // eslint-disable-line no-console
+});
