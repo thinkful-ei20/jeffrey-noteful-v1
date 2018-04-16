839cc745ce191f55f2aeb70a9f826cb9c377cdf5
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-16 15:07:40 -0500

    Create GET notes list endpoint

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const data = require('./db/notes');
 const app = express();
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => res.json(data));
+
+app.get('/api/notes/:id', (req, res) => {
+  res.json(data.find(item => item.id === parseInt(req.params.id)));
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`); // eslint-disable-line no-console
 }).on('error', err => {
