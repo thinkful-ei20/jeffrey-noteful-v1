2c1212b69572924cf5c5a78e3a6422e171b867b5
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-19 15:04:56 -0500

    Refactor POST /notes route to use promises

--- router/notes.router.js
+++ router/notes.router.js
@@ -62,28 +62,27 @@ router.put('/notes/:id', (req, res, next) => {
     });
 });
 
-// Post (insert) an item
 router.post('/notes', (req, res, next) => {
   const { title, content } = req.body;
-
   const newItem = { title, content };
-  /***** Never trust users - validate input *****/
+
   if (!newItem.title) {
     const err = new Error('Missing `title` in request body');
     err.status = 400;
     return next(err);
   }
-
-  notes.create(newItem, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-    } else {
-      next();
-    }
-  });
+  
+  notes.create(newItem)
+    .then(item => {
+      if (item) {
+        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err)
+    });
 });
 
 router.delete('/notes/:id', (req, res, next) => {
